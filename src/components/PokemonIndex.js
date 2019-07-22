79137dbb1a0828d810f9d571b441e21a2eecff46
AustinBH <harlowaustin2@gmail.com>
2019-07-22 10:51:12 -0700

    updated search to implement debounce function

--- src/components/PokemonIndex.js
+++ src/components/PokemonIndex.js
@@ -29,20 +29,20 @@ class PokemonPage extends React.Component {
     this.setState({pokemon: [...this.state.pokemon, pokemon], filteredPokemon: [...this.state.filteredPokemon, pokemon]})
   }
 
-  //_.debounce(() => console.log('🤔'), 500)
+  //)
 
-  handleChange = ev => {
-    if (ev.target.name === 'nameInput') {
+  handleChange = (ev, event) => {
+    if (event.name === 'nameInput') {
       const filteredPokemon = this.state.pokemon.filter(element => {
-        return element.name.includes(ev.target.value)
+        return element.name.includes(event.value)
       })
-      this.setState({ [ev.target.name]: ev.target.value, filteredPokemon })
+      this.setState({ [event.name]: event.value, filteredPokemon })
     }
     else {
       const filteredPokemon = this.state.pokemon.filter(element => {
-          return element.types[0].includes(ev.target.value)
+          return element.types[0].includes(event.value)
         })
-      this.setState({ [ev.target.name]: ev.target.value, filteredPokemon})
+      this.setState({ [event.name]: event.value, filteredPokemon})
     }
   }
 
@@ -54,16 +54,26 @@ class PokemonPage extends React.Component {
         <h1>Pokemon Searcher</h1>
         <br />
         <label>Name Search</label>
-        <Search onSearchChange={this.handleChange} showNoResults={false} name='nameInput' value={this.state.nameInput} />
+        <Search
+          onSearchChange={_.debounce(this.handleChange, 500)}
+          showNoResults={false}
+          name="nameInput"
+          value={this.state.nameInput}
+        />
         <br />
         <label>Type Search</label>
-        <Search onSearchChange={this.handleChange} showNoResults={false} name='typeInput' value={this.state.typeInput} />
+        <Search
+          onSearchChange={_.debounce(this.handleChange, 500)}
+          showNoResults={false}
+          name="typeInput"
+          value={this.state.typeInput}
+        />
         <br />
-        <PokemonCollection pokemon={this.state.filteredPokemon}/>
+        <PokemonCollection pokemon={this.state.filteredPokemon} />
         <br />
-        <PokemonForm addPokemon={this.addPokemon}/>
+        <PokemonForm addPokemon={this.addPokemon} />
       </div>
-    )
+    );
   }
 }
 
