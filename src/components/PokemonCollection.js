8eb44f4cc8623317e08aad599c925799da2a1a82
fut0k0 <cccabell@icloud.com>
2019-07-22 15:41:27 -0700

    Completed lab

--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -3,13 +3,21 @@ import PokemonCard from './PokemonCard'
 import { Card } from 'semantic-ui-react'
 
 class PokemonCollection extends React.Component {
-  render() {
-    return (
-      <Card.Group itemsPerRow={6}>
-        <h1>Hello From Pokemon Collection</h1>
-      </Card.Group>
-    )
-  }
+	
+	displayPokemon = () => {
+		return this.props.searchPokemon.map(pokemon => {
+			return <PokemonCard key={pokemon.id} pokemon={pokemon} />
+		})
+	}
+	
+	render() {
+		return (
+			<Card.Group itemsPerRow={6}>
+				{this.displayPokemon()}
+			</Card.Group>
+		)
+	}
+	
 }
 
-export default PokemonCollection
+export default PokemonCollection;
--- src/components/PokemonIndex.js
+++ src/components/PokemonIndex.js
@@ -5,19 +5,65 @@ import { Search } from 'semantic-ui-react'
 import _ from 'lodash'
 
 class PokemonPage extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Pokemon Searcher</h1>
-        <br />
-        <Search onSearchChange={_.debounce(() => console.log('🤔'), 500)} showNoResults={false} />
-        <br />
-        <PokemonCollection />
-        <br />
-        <PokemonForm />
-      </div>
-    )
-  }
+	
+	constructor() {
+		super();
+		this.state = {
+			allPokemon: [],
+			searchPokemon: []
+		};
+	}
+	
+	componentDidMount() {
+		fetch("http://localhost:3000/pokemon")
+			.then(response => response.json())
+			.then(json => {
+				this.setState({
+					allPokemon: json,
+					searchPokemon: json
+				})
+			})
+	}
+	
+	handleSearchChange = (e, event) => {
+		const searchTerm = event.value;
+		const foundPokemon = this.state.allPokemon.map(pokemon => {
+			if (!!pokemon.name.match(searchTerm)) {
+				return pokemon
+			} else {
+				return null
+			}
+		});
+		const filterPokemon = foundPokemon.filter(pokemon => pokemon !== null);
+		
+		this.setState({
+			searchPokemon: filterPokemon
+		});
+	}
+	
+	addPokemon = pokemon => {
+		this.setState({
+			searchPokemon: [
+				...this.state.searchPokemon,
+				pokemon
+				]
+		});
+	}
+	
+	render() {
+		return (
+			<div>
+				<h1>Pokemon Searcher</h1>
+				<br />
+				<Search onSearchChange={_.debounce(this.handleSearchChange, 500)} showNoResults={false} />
+				<br />
+				<PokemonCollection searchPokemon={this.state.searchPokemon} />
+				<br />
+				<PokemonForm addPokemon={this.addPokemon} />
+			</div>
+		)
+	}
+	
 }
 
-export default PokemonPage
+export default PokemonPage;
